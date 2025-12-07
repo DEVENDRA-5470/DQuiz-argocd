ddc10701993b5ed888e39295326a230c4c236ac0
DEVENDRA-5470 <your_email@example.com>
2025-12-07 22:03:56 +0530

    force push workflows

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("DQuiz API Backend Running");
+  res.send("DQuiz API Backend Running 🧑‍💻✅😊");
 });
 
 app.get("/api/admin/student-count", getStudentCount);
